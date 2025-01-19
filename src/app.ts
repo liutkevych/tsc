c575da6c0acc9521071b5d8357657beaa70ad39a
liutkevych <oli@keepit.com>
2025-01-19 18:20:00 +0100

    Using JS libraries with types. Lodash as an example

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import { ProjectInput } from "./components/project-input-coponent";
 import { ProjectList } from "./components/project-list-component";
+import _ from 'lodash';
 
 new ProjectInput();
 new ProjectList('active');
 new ProjectList('finished');
+
+console.log(_.shuffle([1, 2, 3]));
